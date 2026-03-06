7910e1fbe8b75463dc6be13f87adffc569d5b2c0
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-05 22:24:37 -0700

    Add: Duplicate Integer

---
+++ Unknown/duplicate-integer.c
@@ -0,0 +1,16 @@
+/*
+ * Problem: Duplicate Integer
+ * URL: https://neetcode.io/problems/duplicate-integer
+ * Date: 2026-03-06
+ */
+
+                hashmap[i]=1
+            else:
+                hashmap[i]+=1
+
+        print(hashmap)
+            if i not in hashmap:
+        if any(value > 1 for value in hashmap.values()):
+            return True
+        else:
+            return False
