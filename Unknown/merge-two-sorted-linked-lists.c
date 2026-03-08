921f1e06e9e70e93a0c56468b6c3fbb54944fc81
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-07 21:26:36 -0700

    Add: Merge Two Sorted Linked Lists

---
+++ Unknown/merge-two-sorted-linked-lists.c
@@ -0,0 +1,16 @@
+/*
+ * Problem: Merge Two Sorted Linked Lists
+ * URL: https://neetcode.io/problems/merge-two-sorted-linked-lists
+ * Date: 2026-03-08
+ */
+
+            return list2
+        if list2 is None:
+            return list1
+
+        # edge cases: a list is empty
+        if list1 is None:
+        # can be done inplace
+        # need to iterate through both at the same time, and point val of 1 to val of next
+        dummy=ListNode(0)
+        tail=dummy
