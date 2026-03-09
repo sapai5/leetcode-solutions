174488d4f1ceca6ed13cb85cdfaeb00166b666aa
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-08 20:47:06 -0700

    Update: Middle Of The Linked List

--- Unknown/middle-of-the-linked-list.c
+++ Unknown/middle-of-the-linked-list.c
@@ -4,27 +4,13 @@
  * Date: 2026-03-09
  */
 
-        # get length, divide by two, then math.ceil
-        # double pass through list
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-class Solution:
-#         self.next = next
-#         self.val = val
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
-        # resultant of that is starting pos
-        # use dummy node to start list at that point
-
-        if head is None:
-            return None
-        iterator=head
-        count=0
-        while iterator is not None:
-            count+=1
-            iterator=iterator.next
-        start=count//2
-        iterator=head
-        for _ in range(start):
-            iterator=iterator.next
-        return iterator
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head
+        fast=head
+        while fast and fast.next is not None:
