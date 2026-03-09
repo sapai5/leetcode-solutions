25792d8a1afa6f6bdb36598b6afef49af2ebaff6
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-09 13:41:09 -0700

    Update: Valid Binary Search Tree

--- Unknown/valid-binary-search-tree.c
+++ Unknown/valid-binary-search-tree.c
@@ -4,13 +4,29 @@
  * Date: 2026-03-09
  */
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
+        # need an inorder traversal function (recursive)
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # need to keep track of prev
+#         self.right = right
+
+#         self.val = val
+#         self.left = left
+#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode:
+        # return the recursive function
+
         self.prev = float("-inf")
+
+        def inorder(node):
+            if node is None:
+                return True
+            if not inorder(node.left):
+                return False
+            if node.val <= self.prev:
+                return False
+            self.prev = node.val
+            if not inorder(node.right):
+                return False
+            return True
+        return inorder(root)
