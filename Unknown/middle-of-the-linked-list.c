f5b722e6448cf0230d954f5090db224a1ce9dbc1
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-08 18:26:58 -0700

    Add: Middle Of The Linked List

---
+++ Unknown/middle-of-the-linked-list.c
@@ -0,0 +1,16 @@
+/*
+ * Problem: Middle Of The Linked List
+ * URL: https://neetcode.io/problems/middle-of-the-linked-list
+ * Date: 2026-03-09
+ */
+
+        # get length, divide by two, then math.ceil
+        # double pass through list
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class Solution:
+#         self.next = next
+#         self.val = val
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+        # resultant of that is starting pos
