e323912059b0f66041d95b800308f21637b77ab1
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-08 17:11:17 -0700

    Update: Merge Two Sorted Linked Lists

--- Unknown/merge-two-sorted-linked-lists.c
+++ Unknown/merge-two-sorted-linked-lists.c
@@ -1,16 +1,16 @@
 /*
  * Problem: Merge Two Sorted Linked Lists
  * URL: https://neetcode.io/problems/merge-two-sorted-linked-lists
- * Date: 2026-03-08
+ * Date: 2026-03-09
  */
 
-            return list2
-        if list2 is None:
-            return list1
-
-        # edge cases: a list is empty
-        if list1 is None:
         # can be done inplace
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # need to iterate through both at the same time, and point val of 1 to val of next
-        dummy=ListNode(0)
-        tail=dummy
