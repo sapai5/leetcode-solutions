f342ce20cdad2b6b13458962a20734df416ea70b
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-10 15:51:24 -0700

    Add: Binary Search

---
+++ Unknown/binary-search.c
@@ -0,0 +1,16 @@
+/*
+ * Problem: Binary Search
+ * URL: https://neetcode.io/problems/binary-search
+ * Date: 2026-03-10
+ */
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # use binary search
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
