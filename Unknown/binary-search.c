3d58a244888c1b73660ab293f7ffd7eb211bfc52
sapai <77747518+sapai5@users.noreply.github.com>
2026-03-10 16:40:14 -0700

    Update: Binary Search

--- Unknown/binary-search.c
+++ Unknown/binary-search.c
@@ -8,9 +8,9 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # use binary search
         low = 0
-        high = len(nums)-1
+        high = len(nums) - 1
 
         while low <= high:
             mid = (low + high) // 2
-
             if nums[mid] == target:
+                return mid
